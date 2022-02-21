2649cf24e3821b954ff68e50a03dde6757c1d8a5
Bingji Guo <guobingji@126.com>
2022-02-20 18:59:47 -0600

    update: finish example Quotation component

---
+++ src/components/Quotation.js
@@ -0,0 +1,52 @@
+import { Component } from 'react';
+
+class Quotation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      quotation: {}
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://api.quotable.io/random")
+      .then(res => res.json())
+      .then(
+        (quotation) => {
+          this.setState({
+            isLoaded: true,
+            quotation
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, quotation } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div>
+          <p>{quotation.content}</p>
+          <i>{quotation.author}</i>
+        </div>
+      );
+    }
+  }
+}
+
+export default Quotation; // Don’t forget to use export default!
